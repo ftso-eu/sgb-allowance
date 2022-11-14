208787d8995300eb7bd82528fa953faaaf6fd708
FTSO.EU <78090502+ftso-eu@users.noreply.github.com>
2022-11-14 10:18:11 +0100

    Update header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -30,6 +30,7 @@ class header extends Component {
 		    <h2>SHOW HISTORY AND REVOKE TOKEN ALLOWANCES</h2>
                     <p>supported chains: coston | coston2 | <a href="https://docs.flare.network/dev/reference/network-configs/" target="blank">info</a></p>
                     <p id="addy"></p>
+                    <p id="arrres"></p>
 		    <hr></hr>
                     </div>
 		</div>
