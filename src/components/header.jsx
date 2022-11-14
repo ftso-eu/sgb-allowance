1a3f14e6d1f98e00e0a9ba6bc999c411bccbf7ae
FTSO.EU <78090502+ftso-eu@users.noreply.github.com>
2022-11-14 17:19:48 +0100

    Update header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -33,7 +33,8 @@ class header extends Component {
                      <hr></hr>
 		    <h2>SHOW HISTORY AND REVOKE ALLOWANCES</h2>
                     <p>songbird | coston | coston2 | <a href="https://docs.flare.network/dev/reference/network-configs/" target="blank">info</a></p>
-                    <p id="addy" id="counts" style="font-size:50%;"></p>
+                    <p id="addy" style="font-size:50%"></p>
+		    <p id="counts" style="font-size:50%"></p>
 		</div>
 		</div>
                 <h3 id="loading" hidden>Loading, please wait...</h3>
