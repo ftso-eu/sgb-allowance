61a1278e5d85b0222dd805808a56d5e8ef62fd6a
FTSO.EU <78090502+ftso-eu@users.noreply.github.com>
2022-11-14 15:58:35 +0100

    Update header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -8,7 +8,7 @@ async function onInit() {
         const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
         const account = accounts[0];
         document.getElementById("addy").innerHTML = "connected address: " + account
-        document.getElementById("counts").innerHTML = "approval tx found: " + k
+        document.getElementById("counts").innerHTML = "approval tx found: " + txcounts
         window.ethereum.on('accountsChanged', function (accounts) {
             // Time to reload your interface with accounts[0]!
            });
