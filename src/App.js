14cf5239767cd2b503961f8dfd812f7e495d13b9
FTSO.EU <78090502+ftso-eu@users.noreply.github.com>
2022-11-15 11:11:19 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Web3 from "web3";
 import Header from "./components/header";
 import Allowances from "./components/allowances";
 import React from "react";
-const web3 = new Web3(web3.providers.HttpProvider("https://songbird-api.flare.network/ext/C/rpc"));
+const web3 = new Web3(Web3.providers.HttpProvider("https://songbird-api.flare.network/ext/C/rpc"));
 
 function App() {
     return (
