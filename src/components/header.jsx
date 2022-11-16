7da06c8086b9dbeade343135f43024d3ff878347
FTSO.EU <78090502+ftso-eu@users.noreply.github.com>
2022-11-16 11:29:27 +0100

    Update header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -27,7 +27,7 @@ class header extends Component {
     render() {
         return (
             <div>
-		<div id="bar">
+		<div className="bar">
 		    <p><i>Please beware this is a <strong>Beta</strong> tool in test / canary nets, use it at your own risk.</i></p>
 		</div>
                 <div className="jumbotron">
