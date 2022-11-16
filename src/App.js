1c2d5c53203b74b7015250512b2d00c417ea1d01
FTSO.EU <78090502+ftso-eu@users.noreply.github.com>
2022-11-16 14:17:43 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import Web3 from "web3";
 import Header from "./components/header";
 import Allowances from "./components/allowances";
 import React from "react";
-//let web3 = new Web3(Web3.givenProvider);
-let web3 = new Web3('https://songbird.towolabs.com/rpc');
+let web3 = new Web3(Web3.givenProvider);
+//let web3 = new Web3('https://songbird.towolabs.com/rpc');
 
 function App() {
     return (
