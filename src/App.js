c0ebb9498093ec6eec7c6ade2282f3ac80091d2f
FTSO.EU <78090502+ftso-eu@users.noreply.github.com>
2022-11-16 14:33:56 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,21 @@ import Web3 from "web3";
 import Header from "./components/header";
 import Allowances from "./components/allowances";
 import React from "react";
-let web3 = new Web3(Web3.givenProvider);
-//let web3 = new Web3('https://songbird.towolabs.com/rpc');
+
+//
+import detectEthereumProvider from '@metamask/detect-provider';
+const provider = await detectEthereumProvider();
+if (provider) {
+  console.log("provider" + provider);
+  window.alert("provider" + provider);
+  } else {
+  console.log('Please install MetaMask!');
+  window.alert("provider" + provider);
+}
+let web3 = provider;
+//
+
+//let web3 = new Web3(Web3.givenProvider);
 
 function App() {
     return (
