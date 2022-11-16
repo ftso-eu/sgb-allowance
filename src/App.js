90533f5acc3337fb45ca299345fc6b0fe44fcdf2
FTSO.EU <78090502+ftso-eu@users.noreply.github.com>
2022-11-16 15:14:28 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,19 +4,19 @@ import Allowances from "./components/allowances";
 import React from "react";
 
 //
-import detectEthereumProvider from '@metamask/detect-provider';
-const provider = await detectEthereumProvider();
-if (provider) {
-  console.log("provider" + provider);
-  window.alert("provider" + provider);
-  } else {
-  console.log('Please install MetaMask!');
-  window.alert("provider" + provider);
-}
-let web3 = provider;
+//import detectEthereumProvider from '@metamask/detect-provider';
+//const provider = await detectEthereumProvider();
+//if (provider) {
+//  console.log("provider" + provider);
+//  window.alert("provider" + provider);
+//  } else {
+//  console.log('Please install MetaMask!');
+// window.alert("provider" + provider);
+//}
+//let web3 = provider;
 //
 
-//let web3 = new Web3(Web3.givenProvider);
+let web3 = new Web3(Web3.givenProvider);
 
 function App() {
     return (
