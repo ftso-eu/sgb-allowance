3bc281d795a210ecf2bff79ef6d0a313b6fea62f
FTSO.EU <78090502+ftso-eu@users.noreply.github.com>
2022-11-18 01:33:17 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
         <div>
             <Header />
             <Allowances web3={web3}/>
-            <iframe src="https://widgets.flaremetrics.io/data-provider-metrics/?address=0x010a16c53f33e4d93892f00897965578b55a8cfc&network=songbird&header=true&dark=false&transparent=true" frameborder="0" scrolling="no"  height="650px" width="100%"> </iframe>
+            <iframe src="https://widgets.flaremetrics.io/data-provider-metrics/?address=0x010a16c53f33e4d93892f00897965578b55a8cfc&network=songbird&header=true&dark=false&transparent=true" frameborder="0" scrolling="no"  height="350px" width="100%"> </iframe>
         </div>   
     );
 }
