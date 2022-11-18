3f1599f5414ddfa50950e087f75722df1bb4c24e
FTSO.EU <78090502+ftso-eu@users.noreply.github.com>
2022-11-18 17:29:55 +0100

    Update allowances.jsx

--- src/components/allowances.jsx
+++ src/components/allowances.jsx
@@ -64,11 +64,11 @@ class allowances extends Component {
         let elements = "";
         if(this.state.txs !==  undefined && this.state.chainId !== undefined) {
             const etherscanUrl = getEtherScanPage(this.state.chainId);
-            const ethertxUrl = getEtherTxPage(this.state.chainId);
+            
  
             elements = this.state.txs.map((tx) => {
                 return <Allowance etherscanURL={etherscanUrl} tx={tx} web3={this.props.web3} id={tx.contract} account={this.state.account}/>
-                const ethertxUrlfull = "<a href=" + ethertxUrl + tx.hash + ">hash</a>";
+                
             });
         }
 
