8849e214ec57d2dd76b22a1ace0494662779a629
FTSO.EU <78090502+ftso-eu@users.noreply.github.com>
2022-11-19 12:42:33 +0100

    Update header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -31,7 +31,7 @@ class header extends Component {
                 <div className="jumbotron">
                     <div id="titles">
 		    <h2>SHOW HISTORY AND REVOKE ALLOWANCES</h2>
-                    <h3><a href="https://docs.flare.network/dev/reference/network-configs/" target="_blank">Songbird</a> | <a href="https://github.com/ftso-eu/sgb-allowance/blob/master/README.md" target="_blank">Info <img id="github" alt="GitHub" src="images/github.png" width="20"></img></a></h3>
+                    <h3><a href="https://docs.flare.network/dev/reference/network-configs/" target="_blank">Songbird</a> | <a href="https://github.com/ftso-eu/sgb-allowance/blob/master/README.md" target="_blank">Inf<img id="github" alt="GitHub" src="https://www.kindpng.com/picc/m/613-6133946_github-awesome-logo-svg-hd-png-download.png" width="13"></img></a></h3>
                     <h5 id="addy"></h5>
 		    <h5 id="counts"></h5>
 		</div>
