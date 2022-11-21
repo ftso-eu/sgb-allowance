4b34bf91793b1b384e24dad2b32215c3bdb7e939
FTSO.EU <78090502+ftso-eu@users.noreply.github.com>
2022-11-21 12:42:24 +0100

    Update header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -51,7 +51,7 @@ class header extends Component {
                     <div className="container" onClick={this.revokeAll}>
                         <div className="centered-white"><a id="revokeLink" onClick={this.revokeAll}><strong>REVOKE ALL</strong></a></div>
                     </div>
-                    <hr style="height:2px;border-width:0;color:gray;background-color:gray"></hr>
+                    <hr></hr>
                 </div>
             </div>
         )
