ec2fb89a8aa6e16895a151cbf8058f91ac5358ea
FTSO.EU <78090502+ftso-eu@users.noreply.github.com>
2022-11-21 17:07:54 +0100

    Update allowances.jsx

--- src/components/allowances.jsx
+++ src/components/allowances.jsx
@@ -24,13 +24,13 @@ class allowances extends Component {
                 document.getElementById("loading").hidden = true;
                 //window.alert("TRICK: your approval transactions are sorted chronologically, from newest (top) to oldest (bottom). Pay attention to those with an unlimited spending limit and check if there is a more recent one with a zero limit (revoked): if it doesn't exist for the same spender, then you can revoke the allowance by clicking on the revoke button and signing the transaction. After a few moments you will find a new transaction with zero spending limit which indicates that the spender contract will no longer have allowance for that token on your address.");
             } else {
-                document.getElementById("loading").innerText = "Cool: no allowances to revoke on this account.";
+                document.getElementById("loading").innerText = "Cool! No allowances to revoke fo this address";
                 document.getElementById("revokeAll").hidden = true;
             }
         }).catch((err) => {
-            console.log(err + " - Please report this bug using the links at the bottom of this site, thanks!");
-            //window.alert("This is a Beta version: please re-try using metamask chrome extension from PC to make it work, thanks!");
-            document.getElementById("loading").innerText = "Something went wrong: please check your wallet connection.";
+            console.log(err);
+            document.getElementById("loading").innerText = "sorry, cannot connect your address";
+            window.alert("please check if your wallet is locked, unlock it and reload this page");
             document.getElementById("revokeAll").hidden = true;
         });
     }
