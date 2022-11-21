ba9816c70a99aa90d8d06be1a41665899dc9b5a2
FTSO.EU <78090502+ftso-eu@users.noreply.github.com>
2022-11-21 16:19:51 +0100

    Update dapps.js

--- src/helpers/dapps.js
+++ src/helpers/dapps.js
@@ -1,4 +1,4 @@
 module.exports = {
-    "blazeswap": "https://blazeswap.xyz",
-    "flr finance": "https://flr.finance"
+"blazeswap": "https://blazeswap.xyz",
+"flr finance": "https://flr.finance"
 }
