073854d35e2201ef18d29cd5363da1f396e40c44
FTSO.EU <78090502+ftso-eu@users.noreply.github.com>
2022-11-22 09:33:20 +0100

    Update header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
 import '../App.css';
 import '../helpers/helpers.js';
+
 async function onInit() {
-        await window.ethereum.enable();
-        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-        const account = accounts[0];
-        document.getElementById("addy").innerHTML = "connected address: " + account
-        window.ethereum.on('accountsChanged', function (accounts) {
-            // Time to reload your interface with accounts[0]!
-           });
-    }
+  if (window.ethereum) {
+     await window.ethereum.request({ method: "eth_requestAccounts" });
+     window.web3 = new Web3(window.ethereum);
+     const account = web3.eth.accounts;
+     //Get the current MetaMask selected/active wallet
+     const walletAddress = account.givenProvider.selectedAddress;
+     document.getElementById("addy").innerHTML = "connected address: " + account
+     console.log(`Wallet: ${walletAddress}`);
+  } else {
+   console.log("No wallet");
+  }
+
+//async function onInit() {
+//        await window.ethereum.enable();
+//        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+//        const account = accounts[0];
+//        document.getElementById("addy").innerHTML = "connected address: " + account
+//        window.ethereum.on('accountsChanged', function (accounts) {
+//            // Time to reload your interface with accounts[0]!
+//           });
+//    }
 
 
     onInit();
@@ -32,7 +46,8 @@ class header extends Component {
                     <div id="titles">
 		    <h2>SHOW HISTORY AND REVOKE ALLOWANCES</h2>
                     <h3><a href="https://docs.flare.network/dev/reference/network-configs/" target="_blank">Songbird</a> | <a href="https://docs.flare.network/dev/reference/network-configs/" target="_blank">Coston1+2</a> | <a href="https://github.com/ftso-eu/sgb-allowance/blob/master/README.md" target="_blank">Inf<img id="github" alt="GitHub" src="https://www.kindpng.com/picc/m/613-6133946_github-awesome-logo-svg-hd-png-download.png" width="15"></img></a></h3>
-                    <h5 id="addy"></h5>
+                    <input type="button" value="Connect Wallet" onclick="onInit();">
+		    <h5 id="addy"></h5>
 		    <h5 id="counts"></h5>
 		</div>
 		</div>
