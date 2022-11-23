1692ef8e0733794a26b317c70b2972562e9e80a9
FTSO.EU <78090502+ftso-eu@users.noreply.github.com>
2022-11-23 11:52:52 +0100

    Update header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -48,7 +48,7 @@ class header extends Component {
                 <div className="jumbotron">
                     <div id="titles">
 		    <h1><strong>&#8855;</strong></h1>
-                    <h3><strong>SHOW AND REVOKE ALLOWANC&Xi;</strong></h5>
+                    <h3><strong>SHOW AND REVOKE ALL&#8855;WANC&Xi;</strong></h5>
 		    <h4><a href="https://github.com/ftso-eu/sgb-allowance/blob/master/README.md" target="_blank">Supported chain inf<img id="github" alt="GitHub" src="https://www.kindpng.com/picc/m/613-6133946_github-awesome-logo-svg-hd-png-download.png" width="15"></img></a></h3>
                     <h5 id="addy"></h5>
 		    <h5 id="totcounts"></h5>
