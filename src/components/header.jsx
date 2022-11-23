ab033554b3e47ac7d5c7f98835a6dea9fd3a271a
FTSO.EU <78090502+ftso-eu@users.noreply.github.com>
2022-11-23 21:10:49 +0100

    Update header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -60,7 +60,7 @@ class header extends Component {
 		</div>
                 <h3 id="loading" hidden>Loading, please wait...</h3>
 		<h3 id="partyimg" hidden></h3>
-		<script> window.reload(); </script>
+		<script> window.location.reload(); </script>
                 <div id="revokeAll">
                     <div className="container">
                         <div className="centered"><strong>CONTRACT</strong></div>
