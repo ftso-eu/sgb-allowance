0a1c15dc576add8f9369b7a2f26042df95952f68
FTSO.EU <78090502+ftso-eu@users.noreply.github.com>
2022-11-25 21:58:08 +0100

    Update allowance.jsx

--- src/components/allowance.jsx
+++ src/components/allowance.jsx
@@ -42,7 +42,7 @@ class allowance extends Component {
                 }).catch((err) => {
                     console.log(err)
                     console.log("failed: " + JSON.stringify(err));
-                    window.alert("Ooopsi! Something went wrong, press ok to reload the app. Error detail: " + JSON.stringify(err));
+                    window.alert("Ooopsi! Something went wrong, press ok to reload the app. Please retry to revoke from Chrome and Metamask on your PC! Error details: " + JSON.stringify(err));
                     window.location.reload();
                 });
             } else {
@@ -53,7 +53,7 @@ class allowance extends Component {
                 }).catch((err) => {
                     console.log(err)
                     console.log("failed: " + JSON.stringify(err));
-                    window.alert("Ooopsi! Something went wrong, press ok to reload the app. Error detail: " + JSON.stringify(err));
+                    window.alert("Ooopsi! Something went wrong, press ok to reload the app. Please retry to revoke from Chrome and Metamask on your PC! Error details: " + JSON.stringify(err));
                     window.location.reload();
                 });
             }
