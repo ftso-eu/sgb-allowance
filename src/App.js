4ee88800b4be63a0551f96bc04bd3f4ece63c4c5
FTSO.EU <78090502+ftso-eu@users.noreply.github.com>
2022-11-26 01:04:53 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -23,10 +23,11 @@ function App() {
         <div>
             <Header />
             <Allowances web3={web3}/>
-            <br></br><center>
-            <h4><a href="https://github.com/ftso-eu/sgb-allowance/blob/master/README.md" target="_blank">how it works? get more inf<img id="github" alt="GitHub" src="../images/github-o.png" width="11"></img></a></h4>
+            <center>
+            <div class="jumbotron"><a href="https://github.com/ftso-eu/sgb-allowance/blob/master/README.md" target="_blank">how it works? get more inf<img id="github" alt="GitHub" src="../images/github-o.png" width="11"></img></a>
             <br></br>
             <iframe src="https://widgets.flaremetrics.io/data-provider-metrics/?address=0x010a16c53f33e4d93892f00897965578b55a8cfc&network=songbird&header=true&dark=true&transparent=false" frameborder="0" scrolling="no"  height="450px" width="96%"> </iframe>
+            </div>
             </center>
         </div>   
     );
