76d10bf9e330040557bbf67b7215c77d3f391b15
FTSO.EU <78090502+ftso-eu@users.noreply.github.com>
2022-11-27 23:47:52 +0100

    Update helpers.js

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -125,7 +125,7 @@ export async function getApproveTransactions(query) {
                 }
                  else
                 {
-                    approveObj.allowance = "limited" + \n + (" + time + ")";
+                    approveObj.allowance = "limited" + (" + time + ")";
                     approveObj.allowanceUnEdited = allowance;
                 }
                         
