a94910b380ca6b42be4b286f7d508b1742c7605a
FTSO.EU <78090502+ftso-eu@users.noreply.github.com>
2022-11-28 01:05:58 +0100

    Update header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,31 +2,34 @@ import React, { Component } from 'react';
 import '../App.css';
 import '../helpers/helpers.js';
 
-//async function onInit() {
-//  if (window.ethereum) {
-//     await window.ethereum.request({ method: "eth_requestAccounts" });
-//     window.web3 = new Web3(window.ethereum);
-//     const account = web3.eth.accounts;
-//     //Get the current MetaMask selected/active wallet
-//     const walletAddress = account.givenProvider.selectedAddress;
-//     document.getElementById("addy").innerText = "connected address: " + account
-//     console.log(`Wallet: ${walletAddress}`);
-//  } else {
-//   console.log("No wallet");
-//  }
 
-async function onInit() {
-        await window.ethereum.enable();
-        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+const Web3 = require("web3");
+const ethEnabled = async () => {
+  if (window.ethereum) {
+    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
         const account = accounts[0];
         var accountstart = account.substring(0,5);
 	var accountend = account.substring(account.length - 5);
         document.getElementById("addy").innerText = "connected address: " + accountstart + "....." + accountend;
-        window.ethereum.on('accountsChanged', function (accounts) {
-            window.location.reload() 		
+    window.web3 = new Web3(window.ethereum);
+    return true;
+  }
+  return false;
+}
+
+
+//async function onInit() {
+//        await window.ethereum.enable();
+//        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+//        const account = accounts[0];
+//        var accountstart = account.substring(0,5);
+	//var accountend = account.substring(account.length - 5);
+        //document.getElementById("addy").innerText = "connected address: " + accountstart + "....." + accountend;
+        //window.ethereum.on('accountsChanged', function (accounts) {
+         //   window.location.reload() 		
             // Time to reload your interface with accounts[0]!
-           });
-    }
+        //   });
+  //  }
 
 
     onInit();
