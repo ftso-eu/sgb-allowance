77c54778a290b1222957fb41c60663ef1f68db5c
FTSO.EU <78090502+ftso-eu@users.noreply.github.com>
2022-11-28 01:59:25 +0100

    Update header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -52,7 +52,7 @@ class header extends Component {
                 <div className="jumbotron">
                 <center><img src="../images/allowance.png"></img></center>
                     <div id="titles">
-                    <button onClick={window.location.reload()}><strong>connect</strong></button>
+                    <button onClick={onInit()}><strong>connect</strong></button>
 		    <h2><strong>SHOW AND REVOKE ALLOWANC&Xi;</strong></h2>
                     <h6>Flare, Songbird, Coston1, Coston2, Polygon, Binance Smart Chain, Arbitrum, Optimistic, Ethereum, Ropsten, Rinkeby, Kovan, Fantom.</h6>
 		    <h3><a href="https://github.com/ftso-eu/sgb-allowance/blob/master/README.md" target="_blank">inf<img id="github" alt="GitHub" src="../images/github-o.png" width="15"></img></a></h3>
