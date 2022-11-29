ed762a96012395b9272f2510e4a1afb958035a10
FTSO.EU <78090502+ftso-eu@users.noreply.github.com>
2022-11-29 01:49:33 +0100

    Update helpers.js

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -98,9 +98,7 @@ export async function getApproveTransactions(query) {
         console.log("explorer api return ", dataObj1);
         console.log("explorer api filtered ", dataObj);
         for(let tx of dataObj) {
-        console.log("*INPUT 10: ",dataObj[k].input.slice(0,10));
-        console.log("*INPUT 11: ",dataObj[k].input.slice(1,11));
-        if (dataObj[k].input.substring(1,11) === "0x095ea7b3") {
+        if (dataObj[k].input.slice(0,10) === approvalHash) {
             
                 var a = new Date(dataObj[k].timeStamp * 1000);
                 var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
