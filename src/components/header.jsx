8a157cd7f7c8364dcf242cfc6c8fe6a2f1918ac5
FTSO.EU <78090502+ftso-eu@users.noreply.github.com>
2022-12-08 22:01:45 +0100

    Update header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -89,7 +89,7 @@ class header extends Component {
 		</div>
                 </div>
                 <br></br>
-                <i><h4 id="loading" hidden><br></br>waiting for confirmation <img src="https://media.tenor.com/guhB4PpjrmUAAAAC/loading-loading-gif.gif" width="30px"></img></h4></i>
+                <i><h4 id="loading" hidden><br></br>waiting for confirmation <img src="../../images/loading.gif" width="30px"></img></h4></i>
                 <h6 id="partyimg" hidden></h6>
                 <div className="revokeAll">
                  <div id="revokeAll">
