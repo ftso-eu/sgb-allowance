119cfa3d46d00d7d70f940d0c7a87669deeb22d2
FTSO.EU <78090502+ftso-eu@users.noreply.github.com>
2022-12-09 16:00:07 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Web3 from "web3";
 import Header from "./components/header";
 import Allowances from "./components/allowances";
 import React from "react";
-
+import generatedGitInfo from './generatedGitInfo.json';
 //
 //import detectEthereumProvider from '@metamask/detect-provider';
 //const provider = await detectEthereumProvider();
@@ -29,7 +29,14 @@ function App() {
             <a href="https://github.com/ftso-eu/sgb-allowance/blob/master/README.md" target="_blank">how it works? get more inf<img id="github" alt="GitHub" src="../images/github-o.png" width="10px"></img></a>
             <br></br>
             <br></br>
-      
+          <p>
+            Branch:{' '}
+            <code>{generatedGitInfo.gitBranch}</code>
+          </p>
+          <p>
+            Commit Hash:{' '}
+            <code>{generatedGitInfo.gitCommitHash}</code>
+          </p>
             <iframe src="https://widgets.flaremetrics.io/data-provider-metrics/?address=0x010a16c53f33e4d93892f00897965578b55a8cfc&network=songbird&header=true&dark=true&transparent=false" frameborder="0" scrolling="no"  height="450px" width="96%"> </iframe>
             </div>
             </center>
